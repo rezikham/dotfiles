ed1ed03ec23efca8e74c84646125e95e2ca95d0a
rezikham <rezikham1007@gmail.com>
2022-11-09 12:22:39 +1100

    adjust neovide idle framerate

--- config/nvim/lua/rezikham/core/neovide.lua
+++ config/nvim/lua/rezikham/core/neovide.lua
@@ -7,7 +7,7 @@ vim.cmd([[
   let g:neovide_cursor_vfx_mode = "pixiedust"
   let g:neovide_scale_factor = 0.73
   let g:neovide_refresh_rate = 60
-  let g:neovide_refresh_rate_idle = 1
+  let g:neovide_refresh_rate_idle = 15
   let g:neovide_remember_window_size = v:true
   let g:neovide_cursor_animation_length=0.1
   let g:neovide_cursor_trail_size = 0.2
